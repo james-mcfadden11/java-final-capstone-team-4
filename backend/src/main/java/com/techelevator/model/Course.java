0793ccc199364272c0034eb195e7722d61dffa5f
Frank Giarratani <giarratani@gmail.com>
2021-12-07 16:51:15 -0500

    Courses are transferring over API with GET and POST.

--- backend/src/main/java/com/techelevator/model/Course.java
+++ backend/src/main/java/com/techelevator/model/Course.java
@@ -2,9 +2,44 @@ package com.techelevator.model;
 
 public class Course {
 
-    private String courseName;
+    private String title;
+    private String description;
+    private int difficultyLevel;
+    private double cost;
+
     private int courseID;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDifficultyLevel() {
+        return difficultyLevel;
+    }
+
+    public void setDifficultyLevel(int difficultyLevel) {
+        this.difficultyLevel = difficultyLevel;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
 
     public int getCourseID() {
         return courseID;
@@ -14,16 +49,16 @@ public class Course {
         this.courseID = courseID;
     }
 
-    public Course(String courseName, int courseID) {
-        this.courseName = courseName;
+        public Course(String courseName, int courseID) {
+        this.title = courseName;
         this.courseID = courseID;
     }
 
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
+    public Course(String title, String description, int difficultyLevel, double cost, int courseID) {
+        this.title = title;
+        this.description = description;
+        this.difficultyLevel = difficultyLevel;
+        this.cost = cost;
+        this.courseID = courseID;
     }
 }

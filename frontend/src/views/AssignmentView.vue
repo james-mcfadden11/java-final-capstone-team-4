63056fafc23904065b64bc45225f43b99a3f80e7
James McFadden <jamesmcfadden111@gmail.com>
2021-12-09 09:24:00 -0500

    Renamed 'homework' to 'assignment' and 'content' to 'lesson' across the board, adjusted vue router. Ready to fill in vue components for assignment and lesson

---
+++ frontend/src/views/AssignmentView.vue
@@ -0,0 +1,17 @@
+<template>
+    <assignment-details></assignment-details>
+</template>
+
+<script>
+import AssignmentDetails from '../components/AssignmentDetails.vue'
+
+export default {
+    components: {
+        AssignmentDetails
+    }
+}
+</script>
+
+<style>
+
+</style>

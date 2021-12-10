a833dc8951f6b161770445347455dd31034b9004
Vineeth Mantena <vineeth.mantena325@gmail.com>
2021-12-10 17:19:49 -0500

    styling

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="login" class="text-center">
     <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal White--text">Please Sign In</h1>
+      <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
       <div
         class="alert alert-danger"
         role="alert"

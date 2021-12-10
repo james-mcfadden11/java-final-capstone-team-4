d46f1ffbc0a54da6a2cef3d060011488b9a56727
William Szabo <william.f.szabo@gmail.com>
2021-12-10 16:35:54 -0500

    Styling for dark theme is easier to read now, and I added some interactivity with hovering for a responsive design.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,6 +16,35 @@ body
   background-image: url('./assets/darkthemebg.jpg');
   background-position: center;
   background-size: 100%;
-  text-decoration-color: cornsilk;
+  color: rgb(221, 221, 197);
+  font-family: sans-serif;
 }
+
+a {
+  color: rgb(221, 221, 197);
+}
+
+
+a:visited {
+color: rgb(172, 172, 152);
+}
+
+button {
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 3px;
+  padding: 5px;
+  background-color: rgb(43, 43, 43);
+  color: rgb(221, 221, 197);
+}
+
+button:hover {
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 3px;
+  padding: 6px;
+  background-color: rgb(43, 43, 43);
+  color: rgb(221, 221, 197);
+}
+
 </style>
